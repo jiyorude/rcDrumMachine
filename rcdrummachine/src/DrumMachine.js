5ad8d4d0aefaab9dee19df26be39d80357ff0373
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-02-05 23:00:21 +0100

    05/02 23:00
    Early functionality commit

--- rcdrummachine/src/DrumMachine.js
+++ rcdrummachine/src/DrumMachine.js
@@ -1,48 +1,69 @@
 import React from "react";
 import "./drummachine.min.css";
+import { useState } from "react";
 
 function DrumMachine() {
+  const [currSample, setSample] = useState("-");
+
+  let sampleGrid = {
+    Q: "KICK",
+    W: "KICK 'N HAT",
+    E: "HI-HAT",
+    A: "HEATER",
+    S: "HEATER #2",
+    D: "HEATER #3",
+    Z: "HEATER #4",
+    X: "CLOSED HI-HAT",
+    C: "CLAP",
+  };
+
+  let handleClickEvent = (id) => {
+    setSample(sampleGrid[id]);
+    document.getElementById(id).currentTime = 0;
+    document.getElementById(id).play();
+  };
+
   return (
     <div className="backgroundWrapper">
       <div className="flexWrapper">
         <div id="drum-machine">
           <div id="display">
             <p className="nowPlaying">
-              NOW PLAYING:<span className="play">HI-HAT</span>
+              NOW PLAYING:<span className="play">{currSample}</span>
             </p>
           </div>
           <div id="buttons">
             <div className="row">
-              <button id="Q" className="drum-pad">
-                Q
+              <button onClick={() => handleClickEvent("Q")} id="kick" className="drum-pad">
+                <audio src="./audio/Kick.mp3" className="clip" id="Q"></audio>Q
               </button>
-              <button id="W" className="drum-pad">
-                W
+              <button onClick={() => handleClickEvent("W")} id="kickHat" className="drum-pad">
+                <audio src="./audio/Kick-n-hat.mp3" className="clip" id="W"></audio>W
               </button>
-              <button id="E" className="drum-pad">
-                E
-              </button>
-            </div>
-            <div className="row">
-              <button id="A" className="drum-pad">
-                A
-              </button>
-              <button id="S" className="drum-pad">
-                S
-              </button>
-              <button id="D" className="drum-pad">
-                D
+              <button onClick={() => handleClickEvent("E")} id="hiHat" className="drum-pad">
+                <audio src="./audio/Hi-Hat.mp3" className="clip" id="E"></audio>E
               </button>
             </div>
             <div className="row">
-              <button id="Z" className="drum-pad">
-                Z
+              <button onClick={() => handleClickEvent("A")} id="heater" className="drum-pad">
+                <audio src="./audio/Heater.mp3" className="clip" id="A"></audio>A
               </button>
-              <button id="X" className="drum-pad">
-                X
+              <button onClick={() => handleClickEvent("S")} id="heaterTwo" className="drum-pad">
+                <audio src="./audio/Heater-two.mp3" className="clip" id="S"></audio>S
               </button>
-              <button id="C" className="drum-pad">
-                C
+              <button onClick={() => handleClickEvent("D")} id="heaterThree" className="drum-pad">
+                <audio src="./audio/Heater-three.mp3" className="clip" id="D"></audio>D
+              </button>
+            </div>
+            <div className="row">
+              <button onClick={() => handleClickEvent("Z")} id="heaterFour" className="drum-pad">
+                <audio src="./audio/Heater-four.mp3" className="clip" id="Z"></audio>Z
+              </button>
+              <button onClick={() => handleClickEvent("X")} id="closedHiHat" className="drum-pad">
+                <audio src="./audio/Closed-HiHat.mp3" className="clip" id="X"></audio>X
+              </button>
+              <button onClick={() => handleClickEvent("C")} id="clap" className="drum-pad">
+                <audio src="./audio/Clap.mp3" className="clip" id="C"></audio>C
               </button>
             </div>
           </div>
