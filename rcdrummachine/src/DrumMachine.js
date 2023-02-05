0c8fc168f0a74d1c48f1b76cd068a07cc8d1496a
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-02-05 20:53:46 +0100

    05/02 20.53
    final day commit

--- rcdrummachine/src/DrumMachine.js
+++ rcdrummachine/src/DrumMachine.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import "./drummachine.min.css"
+import React from "react";
+import "./drummachine.min.css";
 
 function DrumMachine() {
   return (
@@ -7,12 +7,49 @@ function DrumMachine() {
       <div className="flexWrapper">
         <div id="drum-machine">
           <div id="display">
-            
+            <p className="nowPlaying">
+              NOW PLAYING:<span className="play">HI-HAT</span>
+            </p>
+          </div>
+          <div id="buttons">
+            <div className="row">
+              <button id="Q" className="drum-pad">
+                Q
+              </button>
+              <button id="W" className="drum-pad">
+                W
+              </button>
+              <button id="E" className="drum-pad">
+                E
+              </button>
+            </div>
+            <div className="row">
+              <button id="A" className="drum-pad">
+                A
+              </button>
+              <button id="S" className="drum-pad">
+                S
+              </button>
+              <button id="D" className="drum-pad">
+                D
+              </button>
+            </div>
+            <div className="row">
+              <button id="Z" className="drum-pad">
+                Z
+              </button>
+              <button id="X" className="drum-pad">
+                X
+              </button>
+              <button id="C" className="drum-pad">
+                C
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default DrumMachine
+export default DrumMachine;
