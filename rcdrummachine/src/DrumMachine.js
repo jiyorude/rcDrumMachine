1aaaf9e0dea264fe7b1b1f3181c2da7c63775ece
Jordy Veenstra <jordy.gaptx@gmail.com>
2023-02-05 16:13:53 +0100

    05/02
    init commit, started over with a new project

---
+++ rcdrummachine/src/DrumMachine.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./drummachine.min.css"
+
+function DrumMachine() {
+  return (
+    <div className="backgroundWrapper">
+      <div className="flexWrapper">
+        <div id="drum-machine">
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DrumMachine
